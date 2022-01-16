3766e9f75dc68bf5c50b2312d77b5575289bb1ec
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-01-16 23:49:26 +0200

    added

---
+++ MultidimensionalArrays-Lab/5.SquareWithMaximumSum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lenght = Console.ReadLine().Split(", ", StringSplitOptions.None).Select(int.Parse).ToArray();
+            int[,] matrix = new int[lenght[0], lenght[1]];
+            int[,] bestSquare = new int[2, 2];
+            int maxSum = Int32.MinValue;
+            for (int row = 0; row < lenght[0]; row++)
+            {
+                int[] line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < lenght[1]; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0) -1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = 0;
+                    if (row + 2 <= lenght[0] && col + 2 <= lenght[1])
+                    {
+                        sum = matrix[row, col] +
+                              matrix[row, col + 1] +
+                              matrix[row + 1, col] +
+                              matrix[row + 1, col + 1];
+                    }
+
+                    if (maxSum < sum)
+                    {
+                        maxSum = sum;
+                        bestSquare = new int[,]
+                        {
+                            { matrix[row, col], matrix[row, col + 1] },
+                            {matrix[row + 1, col], matrix[row + 1, col + 1] }
+                        };
+                    }
+                }
+            }
+
+            Console.WriteLine($"{bestSquare[0,0]} {bestSquare[0,1]}");
+            Console.WriteLine($"{bestSquare[1,0]} {bestSquare[1,1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
+//Create a program that reads a matrix from the console. Then find the
+//biggest sum of the 2x2 submatrix and print it to the console.
+//On the first line, you will get matrix sizes in format rows, columns.
+//In the lines of One next row, you will get elements for each column separated with a comma.
+//Print the biggest top-left square, which you find, and the sum of its elements.
---
+++ MultidimensionalArrays-Lab/6.Jagged-ArrayModification/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace _6.Jagged_ArrayModification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] jaggedMatrix = new int[n][];
+            for (int row = 0; row < n; row++)
+            {
+                //jaggedMatrix[row] = new int[n];
+                jaggedMatrix[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            string[] cmd = Console.ReadLine().Split(); 
+            while (cmd[0] != "END")
+            {
+                int r = int.Parse(cmd[1]);
+                int c = int.Parse(cmd[2]);
+                int value = int.Parse(cmd[3]);
+                switch (cmd[0])
+                {
+                    case "Add":
+                        if (r >= 0 && c >= 0 && r < jaggedMatrix.GetLength(0) && c < jaggedMatrix.GetLength(0))
+                        {
+                            jaggedMatrix[r][c] += value;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid coordinates");
+                        }
+                        break;
+                    case "Subtract":
+                        if (r >= 0 && c >= 0 && r < jaggedMatrix.GetLength(0) && c < jaggedMatrix.GetLength(0))
+                        {
+                            jaggedMatrix[r][c] -= value;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid coordinates");
+                        }
+                        break;
+                }
+                cmd = Console.ReadLine().Split();
+            }
+
+            for (int rows = 0; rows < jaggedMatrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < jaggedMatrix.GetLength(0); cols++)
+                {
+                    Console.Write(jaggedMatrix[rows][cols] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+//Create a program that reads a matrix from the console.
+//On the first line, you will get matrix rows. On the next
+//rows lines, you will get elements for each column separated
+//with space. You will be receiving commands in the following format:
+//Add {row} {col} {value} – Increase the number at the given coordinates with the value.
+//    Subtract {row} { col}
+//{ value} – Decrease the number at the given coordinates by the value.
+//    Coordinates might be invalid. In this case, you should print
+// "Invalid coordinates". When you receive "END" you should print the matrix and stop the program.
