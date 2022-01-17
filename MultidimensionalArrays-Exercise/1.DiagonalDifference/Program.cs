6999ddb51c31dec81011a2f78f757134ade81126
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-01-17 20:26:08 +0200

    finished

--- MultidimensionalArrays-Exercise/1.DiagonalDifference/Program.cs
+++ MultidimensionalArrays-Exercise/1.DiagonalDifference/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _1.DiagonalDifference
 {
@@ -6,7 +7,30 @@ namespace _1.DiagonalDifference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[,] arr = new int[n, n];
+            int leftToRight = 0;
+            int rightToLeft = 0;
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    arr[row, col] = input[col];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                leftToRight += arr[i, i];
+                rightToLeft += arr[i, arr.GetLength(0) - 1 - i];
+            }
+            Console.WriteLine(Math.Abs(leftToRight - rightToLeft));
         }
     }
 }
+//Create a program that finds the difference between the sums of the square matrix diagonals (absolute value).
+//Input
+//    On the first line, you are given the integer N – the size of the square matrix
+//    The next N lines hold the values for every row – N numbers separated by a space
+//Output
+//    Print the absolute difference between the sums of the primary and the secondary diagonal
