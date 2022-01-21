e96b5f931c3689743d38007f12c81c46a4cb9a53
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-01-21 20:57:18 +0200

    +1

---
+++ SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> usernames = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join("\n", usernames));
+        }
+    }
+}
+//Create a program that reads from the console a sequence of N
+//usernames and keeps a collection only of the unique ones. On the first line,
+//you will be given an integer N. On the next N lines, you will receive one username per line.
