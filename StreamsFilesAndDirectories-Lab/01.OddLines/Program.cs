2892efa848723694c240b9462d232daf0b3c6393
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-01-22 23:42:42 +0200

    Streams

---
+++ StreamsFilesAndDirectories-Lab/01.OddLines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
