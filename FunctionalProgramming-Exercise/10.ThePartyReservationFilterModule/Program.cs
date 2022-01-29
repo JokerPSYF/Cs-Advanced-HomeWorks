fbe70874729e5b63d787535176b2bbf370ecd305
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-01-29 16:36:23 +0200

    almost

--- FunctionalProgramming-Exercise/10.ThePartyReservationFilterModule/Program.cs
+++ FunctionalProgramming-Exercise/10.ThePartyReservationFilterModule/Program.cs
@@ -55,19 +55,44 @@ namespace _10.ThePartyReservationFilterModule
                 cmd = Console.ReadLine().Split(";");
             }
 
+            List<string> filteredList = new List<string>();
+
             for (int i = 0; i < names.Length; i++)
             {
+                bool isFiltered = true;
                 foreach (var predicate in predicates.Values)
                 {
                     if (predicate(names[i]))
                     {
-                        names[i] = string.Empty;
+                        isFiltered = false;
                     }
+                }
+
+                if (isFiltered)
+                {
+                    filteredList.Add(names[i]);
                 }
             }
 
-
-            Console.WriteLine(string.Join(" ", names));
+            Console.WriteLine(string.Join(" ", filteredList));
         }
     }
 }
+//You need to implement a filtering module to a party reservation software.
+//First, the Party Reservation Filter Module (PRFM for short) is passed a list with invitations.
+//Next, the PRFM receives a sequence of commands that specify whether you need to add or remove a given filter.
+//    Each PRFM command is in the given format:
+//"{command;filter type;filter parameter}"
+//You can receive the following PRFM commands: 
+//    "Add filter"
+//    "Remove filter"
+//    "Print"
+//The possible PRFM filter types are: 
+//    "Starts with"
+//    "Ends with"
+//    "Length"
+//    "Contains"
+//All PRFM filter parameters will be a string (or an integer only for the "Length" filter).
+//Each command will be valid e.g. you won’t be asked to remove a non-existent filter.
+//The input will end with a "Print" command,
+//after which you should print all the party-goers that are left after the filtration.
