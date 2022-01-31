8f40bce1611e5a2afbe32835805d7cd91cd8382f
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-01-31 22:17:13 +0200

    2 exercises

---
+++ DefiningClasses-Exercise/01.DefineAClassPerson/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public Person()
+        {
+            name = "No name";
+            age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            this.name = name;
+        }
+
+        //public override string ToString()
+        //{
+        //    string str = $"Name: {name} Age: {age}";
+        //    return str;
+        //}
+    }
+}
---
+++ DefiningClasses-Exercise/01.DefineAClassPerson/StartUp .cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person Peter = new Person("Peter", 20);
+            Person George = new Person(18);
+            Person Jose = new Person();
+
+        }
+    }
+}
