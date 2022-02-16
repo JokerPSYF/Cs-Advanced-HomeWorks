bc227fb73571a5a32597416099a6c3949aae58c1
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-02-16 19:40:21 +0200

    almost ready!

---
+++ CsAdvancedRetakeExam-14April2021/01.WarmWinter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.WarmWinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hats = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            List<int> sets = new List<int>();
+
+            while (hats.Any() && scarfs.Any())
+            {
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+
+                if (hat > scarf)
+                {
+                    sets.Add(hats.Pop() + scarfs.Dequeue());
+                }
+                else if (hat < scarf)
+                {
+                    hats.Pop();
+                }
+                else
+                {
+                    scarfs.Dequeue();
+                    hats.Push(hats.Pop() + 1);
+                }
+            }
+
+            int maxSet = sets.Max();
+
+            Console.WriteLine($"The most expensive set is: {maxSet}");
+            Console.WriteLine(string.Join(" ", sets));
+
+        }
+    }
+}
