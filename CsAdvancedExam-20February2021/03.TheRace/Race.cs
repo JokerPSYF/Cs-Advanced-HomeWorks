ffe8f1a4ef02e5ba8580e5bc79b23fc3891d94a5
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-02-17 19:22:50 +0200

    that's ready

---
+++ CsAdvancedExam-20February2021/03.TheRace/Race.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        public List<Racer> Data;
+
+        public List<Racer> Racers { get => Data; set => Data = value; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => Racers.Count;
+
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Racers = new List<Racer>();
+        }
+
+        public void Add(Racer racer)
+        {
+            if (Racers.Count < Capacity)
+            {
+                Racers.Add(racer);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Racer racer = Racers.FirstOrDefault(x => x.Name == name);
+            if (racer != null)
+            {
+                Racers.Remove(racer);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Racer GetOldestRacer() => Racers.OrderByDescending(x => x.Age).First();
+
+        public Racer GetRacer(string name) => Racers.FirstOrDefault(x => x.Name == name);
+
+        public Racer GetFastestRacer() => Racers.OrderByDescending(x => x.Car.Speed).First();
+
+        public string Report()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"Racers participating at {Name}:");
+
+            builder.AppendLine(string.Join(Environment.NewLine, Racers));
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
