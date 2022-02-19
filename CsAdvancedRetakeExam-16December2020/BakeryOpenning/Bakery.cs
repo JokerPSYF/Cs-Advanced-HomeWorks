6bf7698312e3064fb82e76828a31b3a4b7629fad
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-02-19 15:04:50 +0200

    ready for Exam!!!!

---
+++ CsAdvancedRetakeExam-16December2020/BakeryOpenning/Bakery.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        public List<Employee> data;
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Employee> Employees { get => data; set => data = value; }
+        public int Count => Employees.Count;
+
+        public Bakery(string name, int capacity)
+        {
+            Employees = new List<Employee>();
+            Name = name;
+            Capacity = capacity;
+        }
+
+        public void Add(Employee employee)
+        {
+            if (Employees.Count < Capacity)
+            {
+                Employees.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Employee car = Employees.FirstOrDefault(x => x.Name == name);
+            if (car != null)
+            {
+                Employees.Remove(car);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Employee GetOldestEmployee() => Employees.OrderByDescending(x => x.Age).First();
+
+        public Employee GetEmployee(string name) => Employees.FirstOrDefault(x => x.Name == name);
+
+        public string Report()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"Employees working at Bakery {Name}:");
+
+            builder.AppendLine(string.Join(Environment.NewLine, Employees));
+
+            return builder.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ CsAdvancedRetakeExam-16December2020/BakeryOpenning/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace BakeryOpenning
+{
+    public class Employee
+    {
+        //Name: string
+        //Age: int
+        //Country: string
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public Employee(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public override string ToString() => $"Employee: {Name}, {Age} ({Country})";
+      
+    }
+}
